806060aab7abfbe6380e48b5e961561919798438
David Bonilla <thegrindnet@gmail.com>
2026-06-16 01:11:35 -0600

    Build FormAddFriend, FormSplitBill, Button components

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -57,7 +60,49 @@ function Friend({ friend }) {
       )}
       {friend.balance === 0 && <p c>You and {friend.name} are even</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👨🏽‍🤝‍👨🏻Friend name</label>
+      <input type="text" />
+      <label>🌆 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>🕺 Your expense</label>
+      <input type="text" />
+
+      <label>👨🏽‍🤝‍👨🏻 X's expense</label>
+      <input type="text" disabled />
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
   );
 }
