d71a1265cab5a1e3b859588e12c4f018e4e54f56
David Bonilla <thegrindnet@gmail.com>
2026-06-28 01:12:17 -0600

    Update handleSplitBill handler in App component, finished logic

--- src/App.js
+++ src/App.js
@@ -50,7 +50,13 @@ export default function App() {
   }
 
   function handleSplitBill(value) {
-    console.log(value);
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
   }
 
   return (
